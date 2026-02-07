43caa4b5e18075d8ad146cc5ef1822c92835db82
Youcef <youcefcopy@gmail.com>
2026-02-07 10:47:10 +0100

    Directives: v-if

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@ export default {
   data() {
     return {
       name: 'Jake the Dog',
+      status: false,
     }
   },
 }
 </script>
 
 <template>
-  <h1>Hello dude</h1>
+  <h1>Hello {{ name }}</h1>
+  <p v-if="status">You're here!!!</p>
 </template>
 
 <style scoped></style>
