0dbb2bb28685ea6f99ff656e0b974b0ccb6b7a55
Youcef <youcefcopy@gmail.com>
2026-02-07 11:11:20 +0100

    if directives & v-on event listener

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,16 @@ function change() {
 }
 </script>
 <template>
-  <h1>Hello {{ name }}</h1>
-  <p v-if="status">You're here!!!</p>
-  <p v-else>You are not here</p>
-  <button @click="change">Click to Switch</button>
+  <div class="condtiom-directives">
+    <h1>Hello {{ name }}</h1>
+    <p v-if="status === true">User is Avtive</p>
+    <p v-else>User is not active</p>
+    <button @click="change">Click to Switch</button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+p {
+  font-size: 3rem;
+}
+</style>
