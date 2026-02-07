08b73a594bf30286b858b2fc2c45bb475284cee3
Youcef <youcefcopy@gmail.com>
2026-02-07 10:42:30 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+export default {
+  data() {
+    return {
+      name: 'Jake the Dog',
+    }
+  },
+}
+</script>
+
+<template>
+  <h1>Hello dude</h1>
+</template>
+
+<style scoped></style>
