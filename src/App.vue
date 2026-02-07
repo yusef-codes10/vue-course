6a453ad501cc415d0f4918c13cb7f3dbf0a69dc1
Youcef <youcefcopy@gmail.com>
2026-02-07 10:52:30 +0100

    clickabel btn finally

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
-<script>
-export default {
-  data() {
-    return {
-      name: 'Jake the Dog',
-      status: false,
-    }
-  },
+<script setup>
+import { ref } from 'vue'
+
+// declare reactive variables
+const name = ref('Jake the Dog')
+const status = ref(true)
+
+function change() {
+  status.value = !status.value
 }
 </script>
-
 <template>
   <h1>Hello {{ name }}</h1>
   <p v-if="status">You're here!!!</p>
+  <p v-else>You are not here</p>
+  <button @click="change">Click to Switch</button>
 </template>
 
 <style scoped></style>
